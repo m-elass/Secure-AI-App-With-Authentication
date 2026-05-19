583ec67a2af05134189ab35c090567a0368aca48
Mohamed <m.elassouti.2025@alumnos.urjc.es>
2026-05-19 18:54:15 +0200

    db changed

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -20,7 +20,7 @@ def create_challenge_quota(db: Session, user_id: str):
 def reset_quota_if_needed(db: Session, quota: models.ChallengeQuota):
     now = datetime.now()
     if now - quota.last_reset_date > timedelta(hours=24):
-        quota.quota_remaining = 20
+        quota.quota_remaining = 30
         quota.last_reset_date = now
         db.commit()
         db.refresh(quota)
